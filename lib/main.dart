6e13c41590814ccb862b8ee510d08af5f9b987ef
Mahmoud Adel <mahmoudadela413@gmal.com>
2024-02-06 22:42:54 -0800

    add Simple Bloc Observer to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/blocs/auth_bloc/auth_bloc.dart';
 import 'package:chat_app/pages/chatPage.dart';
@@ -5,16 +7,23 @@ import 'package:chat_app/pages/cubit/chat_cubit.dart';
 import 'package:chat_app/pages/home.dart';
 import 'package:chat_app/pages/loginPage.dart';
 import 'package:chat_app/pages/registerPage.dart';
+import 'package:chat_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const ScholarChat());
+  BlocOverrides.runZoned(
+    () {
+      runApp(const ScholarChat());
+    },
+    blocObserver: SimpleBlocObserver(),
+  );
+  // runApp(const ScholarChat());
 }
 
 class ScholarChat extends StatelessWidget {
@@ -27,17 +36,21 @@ class ScholarChat extends StatelessWidget {
         providers: [
           // BlocProvider(create: (context) => LoginCubit(),),
           // BlocProvider(create: (context) => RegisterCubit(),),
-          BlocProvider(create: (context) => AuthBloc(),),
-          BlocProvider(create: (cntext) => ChatCubit(),),
+          BlocProvider(
+            create: (context) => AuthBloc(),
+          ),
+          BlocProvider(
+            create: (cntext) => ChatCubit(),
+          ),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData.dark(),
           routes: {
             HomePage.id: (context) => const HomePage(),
-            ChatPage.id: (context) =>  ChatPage(),
-            LoginPage.id: (context) =>  LoginPage(),
-            RegisterPage.id: (context) =>  RegisterPage(),
+            ChatPage.id: (context) => ChatPage(),
+            LoginPage.id: (context) => LoginPage(),
+            RegisterPage.id: (context) => RegisterPage(),
           },
           initialRoute: HomePage.id,
         ),
